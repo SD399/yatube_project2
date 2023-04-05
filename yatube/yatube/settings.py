a14d7da6d43d3e84079dd1246812e42af2f689b2
SEREGA399 <ben4press@gmail.com>
2023-04-05 20:38:43 +0300

    sprint3

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -51,10 +52,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'yatube.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,6 +83,8 @@ DATABASES = {
     }
 }
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# Или так:
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
